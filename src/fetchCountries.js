a6d8604e0871f28c4ee7ac74470b23bf002fa1b8
basiaz79 <bzasowski79@gmail.com>
2024-06-24 21:35:01 +0200

    HW 10

---
+++ src/fetchCountries.js
@@ -0,0 +1,22 @@
+import Notiflix from 'notiflix';
+
+const COUNTRIES_API_URL = `https://restcountries.com/v3.1/name/`;
+
+const fetchCountries = async name => {
+  try {
+    const response = await fetch(
+        COUNTRIES_API_URL +
+          name +
+          '?fields=name,capital,populatoin,flags,langualges'
+    );
+
+    if (response.status == 200) {
+      let json = await response.json();
+      return json;
+    }
+  } catch (error) {
+    throw new Error(response.status);
+  }
+};
+
+export { fetchCountries };
