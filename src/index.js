67aa3f62be6dfcc4199c8222c4aa0e88a3e39594
basiaz79 <bzasowski79@gmail.com>
2024-06-25 13:09:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,99 +1,99 @@
-import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import { debounce } from 'lodash.debounce';
-import { Notify} from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-fetchCountries('pol')
+fetchCountries('pol');
 
 const inputCountry = document.querySelector('input#search-box');
-const listCountry = document.querySelector('.country-list');
-const infoCountry = document.querySelector('.country-info');
+const listCountry = document.querySelector('#country-list');
+const infoCountry = document.querySelector('#country-info');
 
-const DEBOUNCE_DELAY = 300
+const DEBOUNCE_DELAY = 300;
 
-//search countries database
+// Search countries database
 function inputHandler(event) {
-    const searchInput = event.target.value.trim();
+  const searchInput = event.target.value.trim();
 
-    cleanCountry()
-    cleanListCountry()
+  cleanCountry();
+  cleanListCountry();
 
-    fetchCountries(searchInput)
-        .then(data => {
-            if (data.lenght > 10) {
-                Notify.info('Too many matches found. Please enter a more specific name');
-                return;
-            }
-            countryDataMarkup(data);
-        })
-        .catch(err => {
-            Notify.failure('Oops, there is no country with that name');
-        });
-};
+  fetchCountries(searchInput)
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+        return;
+      }
+      countryDataMarkup(data);
+    })
+    .catch(err => {
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
 
-//creating countries list markup
+// Creating countries list markup
 function createListMarkup(data) {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li class="country-list_item" data-country='${name.common}'><img class="country-list_image" src="${flags.svg}" alt="${name.common}" height="40px"/><p class="country-list_post">${name.common}</p></li>`
+    )
+    .join('');
+}
 
-    return data
-        .map(({ name, flags }) =>
-            `<li class="country-list_item" data-country='${name.common}'><img class=country-list_image" src="${flags.svg}" alt="${name.common}" height="40px" "/><p class="country-list_post">${name.common}</p></li>
-        )
-        .join('');
+// Creating country info markup
+function createDataMarkup(data) {
+  const countryEl = data[0];
+  const { name, capital, population, flags, languages } = countryEl;
+  return `
+        <li class="country_item">
+            <div class="country_flag-name-container">
+                <img src="${flags.svg}" alt="${name.common}" height="30px"/>
+                <h2 class="country_title">${name.official}</h2>
+            </div>
+            <p><b>Capital:</b> ${capital}</p>
+            <p><b>Population:</b> ${population}</p>
+            <p><b>Languages:</b> ${Object.values(languages).join(', ')}</p>
+        </li>
+    `;
+}
 
-};
-
-//creating country info markup
-function crateDataMarkup(data) {
-    const countryE1 = data[0]};
-    const {name, capital, population, flags, languages } = countryE1;
-        return `
-                <li class="country_item">
-                    <><div class="country_flag-name-container">
-                ing src="${flags.svg}" alt="${country.common}" height="30px"/></p><h2 class="country_title">${country.official}</h2></>
-                    </div>
-                    <p><b>Capital:<b> ${capital}
-                    <p><b>Population:</b> ${population}</p>
-                    <p><b>Lanbuages:</b> ${Object.values(data[0].languages)}</p>
-                </li>
-                `;
-};
-
-//rendering
+// Rendering
 function countryDataMarkup(data) {
-    if (data.lenght === 1) {
-        const DataMarkup = createDataMarkup(data);
-        infoCountry.innerHTML = dataMarkup;
-    } else {
-        const listMarkup = createListMarkup(data);
-        listCountry.innerHTML = listMarkup;
+  if (data.length === 1) {
+    const dataMarkup = createDataMarkup(data);
+    infoCountry.innerHTML = dataMarkup;
+  } else {
+    const listMarkup = createListMarkup(data);
+    listCountry.innerHTML = listMarkup;
 
-        //add click to a country on list
-        const listCountryItem = document.querySelectorAll('li');
+    // Add click event to a country on list
+    const listCountryItems = document.querySelectorAll('li');
 
-        listCountryItem.forEach(item => {
-            item.addEventListener('click', event => {
-                const clickedCountry = event.currentTarget.dataset.country;
-                //searches the database by clicked country
-                const wantedCountry = data.filter(
-                    country => country.name.common === clickedCountry
-                );
+    listCountryItems.forEach(item => {
+      item.addEventListener('click', event => {
+        const clickedCountry = event.currentTarget.dataset.country;
+        // Searches the database by clicked country
+        const wantedCountry = data.filter(
+          country => country.name.common === clickedCountry
+        );
 
-                infoCountry.innerHTML = createDataMarkup(wantedCountry);
-                console.log('item clicked', clickedCountry);
-                cleanListCountry();
-            });
-        });
-    };
-};
-            
-//input event addEventListener
-inputCountry.addEventListener('input',debounce(inputHandler, DEBOUNCE_DELAY));
+        infoCountry.innerHTML = createDataMarkup(wantedCountry);
+        cleanListCountry();
+      });
+    });
+  }
+}
 
-//clear markup
+// Input event addEventListener
+inputCountry.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
+
+// Clear markup
 function cleanCountry() {
-    infoCountry.innerHTML = '';
-};
+  infoCountry.innerHTML = '';
+}
 
 function cleanListCountry() {
-    listCountry.innerHTML = '';
-};
+  listCountry.innerHTML = '';
+}
